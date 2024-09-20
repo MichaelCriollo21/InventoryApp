1813c6858b1d95addc45bc9e0d3bcb7a6ff4c23b
Michael Criollo <macg2151@gmail.com>
2024-09-19 23:28:13 -0500

    Menu

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+  
+  pages = [
+    { title: 'Inicio', url: '/main/home', icon: 'home-outline' },
+    { title: 'Perfil', url: '/main/profile', icon: 'person-outline' },
+  ]
+
+  router = inject(Router)
+  currentPath: string = '';
 
   ngOnInit() {
+    this.router.events.subscribe((event: any) => {
+      if(event?.url) this.currentPath = event.url;
+    })
   }
 
+  user(): User {
+    return this.utilsSvc.getFromLocalStorage('user');
+  }
+  
+  signOut(){
+    this.firebaseSvc.signOut();
+  }
 }
